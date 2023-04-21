50dd5c2b83a334741a6084e2b0688d659315852d
tyouthfor <92776347+tyouthfor@users.noreply.github.com>
2023-04-21 23:59:26 +0800

    Update Top.v

--- 实验二：单周期CPU (1)/代码/Top.v
+++ 实验二：单周期CPU (1)/代码/Top.v
@@ -19,47 +19,47 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-// ¶¥²ãÄ£¿é
+// é¡¶å±‚æ¨¡å—
 module Top(
     input clk,
     input rst,
     
-    // Mux µÄ sel ĞÅºÅ
+    // Mux çš„ sel ä¿¡å·
     output RegDst,
     output ALUSrc,
     output MemtoReg,
     output Branch,
     output Jump,
     
-    // ¶ÁĞ´¿ØÖÆĞÅºÅ
+    // è¯»å†™æ§åˆ¶ä¿¡å·
     output MemRead,
     output MemWrite,
     output RegWrite,
     
-    // ALU¿ØÖÆĞÅºÅ
+    // ALUæ§åˆ¶ä¿¡å·
     output [2:0] ALUControl,
     
-    // Æß¶ÎÊıÂë¹ÜÏÔÊ¾
+    // ä¸ƒæ®µæ•°ç ç®¡æ˜¾ç¤º
     output [7:0] an,
     output [6:0] seg
     );
     
-    // Ê±ÖÓ·ÖÆµ
+    // æ—¶é’Ÿåˆ†é¢‘
     wire clk_div;
     Clock_divide Clock_divede (.clk(clk), .clk_div(clk_div));
     
-    // PC×ÔÔö
+    // PCè‡ªå¢
     wire [31:0] next_instruction_addr;
     wire [31:0] curr_instruction_addr;
     wire instruction_en;
     PC PC (.clk(clk_div), .rst(rst), .cin(next_instruction_addr), .cout(curr_instruction_addr), .instruction_en(instruction_en));
     Adder PC_increment (.a(curr_instruction_addr), .b(32'h4), .c(next_instruction_addr));
     
-    // È¡Ö¸
+    // å–æŒ‡
     wire [31:0] instruction;
     blk_mem_gen_0 Instruction_memory (.clka(clk_div), .ena(instruction_en), .addra(curr_instruction_addr), .douta(instruction));
     
-    //ÒëÂë
+    // è¯‘ç 
     Controller Controller
     (
     .op(instruction[31:26]), .funct(instruction[5:0]), 
@@ -68,7 +68,7 @@ module Top(
     .ALUControl(ALUControl)
     );
     
-    //Æß¶ÎÊıÂë¹ÜÏÔÊ¾
+    // ä¸ƒæ®µæ•°ç ç®¡æ˜¾ç¤º
     Digital_Tube Digital_Tube (.clk(clk), .rst(rst), .display(instruction), .an(an), .seg(seg));
     
 endmodule
